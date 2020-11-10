c219e2a851db8bc2d38f671c4d230eb1e880665c
Dillon Walls <dillon.walls@slashdotmedia.com>
2020-11-10 17:22:50 -0500

    fixup! walmart ps5/xsx and notifications for tarjae

--- TargetAutoAddOrder.user.js
+++ TargetAutoAddOrder.user.js
@@ -62,7 +62,7 @@ setInterval(function(){ //refresh function
     console.log("reload, hasFocus", document.hasFocus());
 
     location.reload();
-}, 5000); //refreshes every 3 minutes
+}, 18000); //refreshes every 3 minutes
 
 /* script is a very minor modification of the existing auto add preorder script, which I will leave as is for future releases
 and also to simplify explaining it's use. 
