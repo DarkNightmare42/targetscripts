37bd698cd1e2351fa337038b35d5b499c0b4fba1
DarkNightmare42 <47510339+DarkNightmare42@users.noreply.github.com>
2020-11-21 22:00:05 -0500

    Update walmartCheckout.user.js

--- walmartCheckout.user.js
+++ walmartCheckout.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         walmartCheckout
 // @namespace    http://example.tld
-// @version      1.0.0
+// @version      1.0.1
 // @description  walmartCheckout
 // @author       DarkNightmare42, dill0wn
 // @updateUrl    https://github.com/DarkNightmare42/targetscripts/raw/main/walmartCheckout.user.js
@@ -28,20 +28,31 @@ setInterval(function(){
 setInterval(function(){
     var placeOrder = document.querySelectorAll('[data-tl-id="button"]');
     var cvv = document.querySelectorAll('[inputid="cvv-verify-1"]');
-    if(placeOrder){
+    var inCart = document.querySelectorAll('[analyticsid="cart-btn-save-for-later"]');
+    var method = document.querySelectorAll('[data-automation-id="fulfillment-shipping-group-0"]');
+    var addAddress = document.querySelectorAll('[data-automation-id="new-address-tile-add-new-address"]');
+    var propose = document.querySelectorAll('[name="proposeDonation"]');
+
+    if(inCart){
+        placeOrder[0].click();
+        if(method){
+            placeOrder[0].click();
+            if(addAddress){
+                placeOrder[0].click();
+                if(cvv){
+                    ("field-input field-input--secondary").val ("insert_cvv_here");
+                    placeOrder[0].click();
+                }
+            }
+        }
+    }
+    if(propose){
         placeOrder[0].click();
     }
-    placeOrder[0].click();
-    placeOrder[0].click();
-    if(cvv){
-        ("field-input field-input--secondary").val ("insert_cvv_here");
-    }
-    placeOrder[0].click();
-    placeOrder[0].click();
 }, 250); //refreshes every minute
 
 setInterval(function(){
     //script refresh
 }, 60000);
 
-//script is in the testing stages, use at your own risk 
+//untested, should be functional
