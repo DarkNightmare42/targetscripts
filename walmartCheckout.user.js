937d260842869d9970a43cd2afe289fe9b42ce54
DarkNightmare42 <47510339+DarkNightmare42@users.noreply.github.com>
2020-11-25 15:52:17 -0500

    Update walmartCheckout.user.js

--- walmartCheckout.user.js
+++ walmartCheckout.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         walmartCheckout
 // @namespace    http://example.tld
-// @version      1.0.1
+// @version      1.0.2
 // @description  walmartCheckout
 // @author       DarkNightmare42, dill0wn
 // @updateUrl    https://github.com/DarkNightmare42/targetscripts/raw/main/walmartCheckout.user.js
 // @downloadUrl  https://github.com/DarkNightmare42/targetscripts/raw/main/walamartCheckout.user.js
 // @match        https://www.walmart.com/cart
-// @match        https://www.walmart.com/checkout/*
+// @match        https://www.walmart.com/checkout*
 // @grant        none
 // ==/UserScript==
 
@@ -27,20 +27,21 @@ setInterval(function(){
 
 setInterval(function(){
     var placeOrder = document.querySelectorAll('[data-tl-id="button"]');
-    var cvv = document.querySelectorAll('[inputid="cvv-verify-1"]');
+    var con = document.querySelectorAll('[data-automation-id="persistent-footer-continue"]');
+    var cvv = document.querySelectorAll('[class="field-input field-input--secondary"]');
     var inCart = document.querySelectorAll('[analyticsid="cart-btn-save-for-later"]');
-    var method = document.querySelectorAll('[data-automation-id="fulfillment-shipping-group-0"]');
+    var method = document.querySelectorAll('[aria-label="Continue to Delivery Address"]');
     var addAddress = document.querySelectorAll('[data-automation-id="new-address-tile-add-new-address"]');
     var propose = document.querySelectorAll('[name="proposeDonation"]');
 
     if(inCart){
         placeOrder[0].click();
         if(method){
-            placeOrder[0].click();
+            con[0].click();
             if(addAddress){
-                placeOrder[0].click();
+                con[0].click();
                 if(cvv){
-                    ("field-input field-input--secondary").val ("insert_cvv_here");
+                    document.getElementByName("cvv").value=("cvv-placeholder");
                     placeOrder[0].click();
                 }
             }
@@ -49,10 +50,8 @@ setInterval(function(){
     if(propose){
         placeOrder[0].click();
     }
-}, 250); //refreshes every minute
+}, 60000); //refreshes every minute
 
 setInterval(function(){
     //script refresh
 }, 60000);
-
-//untested, should be functional
